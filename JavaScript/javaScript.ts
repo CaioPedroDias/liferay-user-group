5e2b889b65eb0a43e15d1e0e6517e492f970942c
CaioP <caiopedro28@gmai.com>
2024-10-21 18:05:18 -0300

    first commit

---
+++ JavaScript/javaScript.ts
@@ -0,0 +1,9 @@
+const loginBox = document.querySelector<HTMLElement>('.login-box');
+if (loginBox) {
+    loginBox.style.backgroundColor = '#0b5fff';
+}
+
+const header = document.querySelector<HTMLElement>('#header');
+if (header) {
+    header.style.backgroundColor = '#0b5fff';
+}
